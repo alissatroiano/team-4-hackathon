c8e1bd0d36c52edf32321df70cdf0354bf6d471a
Stefan Dworschak <stef.dworschak@gmail.com>
2021-12-13 13:46:47 +0000

    Fix ordering of the dates

--- covidxmas/dates/urls.py
+++ covidxmas/dates/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('create_calendar/', create_calendar, name='create_calendar'),
     path('edit_dates/<str:calendar_id>/', edit_dates, name='edit_dates'),
     path('settings/<str:calendar_id>/', settings, name='settings'),
-    path('/delete/<str:calendar_id>/', delete_calendar, name='delete_calendar'),
+    path('delete/<str:calendar_id>/', delete_calendar, name='delete_calendar'),
     path('view_calendars/', view_calendars, name='view_calendars'),
     path('view_public_calendars/', view_public_calendars, name='view_public_calendars'),
 ]
